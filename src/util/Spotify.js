b394cc03e1ee1af347841993cdc9b31fe300f07e
Schmucki Peter <pschm@ruf.ch>
2018-07-27 15:29:38 +0200

    Implemented unstable version of access token save

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-let accessToken, expiresIn;
+let accessToken;
 const redirectURI = 'http://localhost:3000/';
 const clientID = '03ab43f6ef994bd9983d285284a7be44';
 const searchBase = 'https://api.spotify.com/v1/';
@@ -10,22 +10,35 @@ const Spotify = {
     if(accessToken) {
       return accessToken;
     }
-    let tokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    let expireMatch = window.location.href.match(/expires_in=([^&]*)/);
-    if (tokenMatch && expireMatch) {
-      accessToken = tokenMatch[1];
-      expiresIn = parseInt(expireMatch[1], 10);
+    if(localStorage.getItem('spotify_token', '') !== '') {
+      var expires = 0 + localStorage.getItem('spotify_expires', '0');
+				if ((new Date()).getTime() > expires) {
+					return '';
+				}
+      return localStorage.getItem('spotify_token', '');
+    }
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
+      this.setAccessToken(accessToken, expiresIn);
       return accessToken;
-    } else {
+    } else  {
       const fallbackURL =`${accessBase}?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
       window.location = fallbackURL;
     }
   },
 
+  setAccessToken(token, expires) {
+    localStorage.setItem('spotify_token', token);
+		localStorage.setItem('spotify_expires', (new Date()).getTime() + expires);
+  },
+
   search(term) {
-    let token = this.getAccessToken();
+    let token = Spotify.getAccessToken();
     return fetch(`${searchBase}search?type=track&q=${term}`, {
       headers: {Authorization: `Bearer ${token}`}
     }).then(response => {
